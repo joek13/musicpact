73d00858a01b6c1fa60975bfc5eb9fbcbc7bb280
Joe Kerrigan <joek1301@gmail.com>
2021-03-27 14:32:58 -0400

    commit db secrets ahhhh

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require('express');
-const app = express();
-const port = 8080;
+const nconf = require('nconf');
+const MongoClient = require("mongodb").MongoClient;
 
-app.use(express.static("public"));
+nconf.argv()
+    .env()
+    .file("config.json");
 
-app.listen(port, () => {
-    console.log(`Application listening at http://localhost:${port}`)
+uri = nconf.get("DB_URI");
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    const db = client.db("userdb");
+    const app = express();
+    const port = 8080;
+
+    app.use(express.static("public"));
+
+    app.get("/count", async (req, res) => {
+        db.collection("counter").insertOne({
+            sender: req.ip
+        });
+
+        var count = await db.collection("counter").count();
+        res.send("Count is: " + count);
+    });
+
+    app.listen(port, () => {
+        console.log(`Application listening at http://localhost:${port}`)
+    });
 });
+
+
